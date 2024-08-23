6b0682fd70673da58cd3d894ddb5edcffde5e68e
ashutoshrai204 <97841241+ashutoshrai204@users.noreply.github.com>
2024-08-24 05:05:22 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 
-export const server = "http://localhost:5001";
+export const server = "https://gurukulserver.onrender.com";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
